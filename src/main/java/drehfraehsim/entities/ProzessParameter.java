8114dc90d890d4651bb319ceecfa8735d72cf2b3
Joachim Krallmann <joachim.krallmann@tu-dortmund.de>
2022-05-17 09:27:14 +0200

    Beispielprozessparamater hinzu

--- src/main/java/drehfraehsim/entities/ProzessParameter.java
+++ src/main/java/drehfraehsim/entities/ProzessParameter.java
@@ -5,7 +5,17 @@ import drehfraehsim.entities.ProzessParameter.WerkstückParameter;
 import drehfraehsim.entities.ProzessParameter.WerkzeugParameter;
 
 public record ProzessParameter(OperationsParameter operationsParameter, WerkzeugParameter werkzeugParameter, WerkstückParameter werkstückParameter) {
-	public static record WerkstückParameter(double radius, double höhe) {}
+	public static record OperationsParameter(double einstellWinkel, double schnittGeschwindigkeit, double schnittTiefe, double vorschub, double zielDurchmesser, double bearbeitungsLänge) {
+		public OperationsParameter {
+			if (einstellWinkel < 0 || einstellWinkel > 360) {
+				throw new IllegalArgumentException("Einstellwinkel muss zwischen 0 und 360 sein, gegeben: " + einstellWinkel);
+			}
+		}
+	}
+	public static record WerkstückParameter(double radius, double länge) {}
 	public static record WerkzeugParameter(double höhe, double breite) {}
-	public static record OperationsParameter(double einstellWinkel, double schnittGeschwindigkeit, double schnittTiefe, double vorschub, double zielDurchmesser, double bearbeitungsLänge) {}
+
+	public static ProzessParameter beispiel() {
+		return new ProzessParameter(new OperationsParameter(95, 1, 1, 1, 5, 50), new WerkzeugParameter(1, 1), new WerkstückParameter(10, 100));
+	}
 }
