9ab2fcf5d909fa766404152a1dc5a7a889c03f23
Joachim Krallmann <joachim.krallmann@tu-dortmund.de>
2022-05-17 09:27:31 +0200

    Führe Simulator mit Beispiel ProzessParamater aus

--- src/main/java/drehfraehsim/services/CLI.java
+++ src/main/java/drehfraehsim/services/CLI.java
@@ -1,10 +1,12 @@
 package drehfraehsim.services;
 
+import drehfraehsim.entities.ProzessParameter;
+
 public class CLI {
 
 	public void process(String[] args) {
 		// TODO parsen und so
-		new Simulator(null).run();
+		new Simulator(ProzessParameter.beispiel()).run();
 	}
 
 }
