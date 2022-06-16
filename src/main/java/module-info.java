5a68597f991546933f039e52a642ab427585bc4f
Mark Joachim Krallmann <joachim.krallmann@protonmail.com>
2022-06-16 16:48:48 +0200

    lwjgl testweise hinzu

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module drehfraehsim {
 	requires commons.cli;
 	requires javafx.graphics;
+	requires org.lwjgl;
+	requires org.lwjgl.opengl;
+	requires org.lwjgl.glfw;
 
 	exports drehfraehsim to javafx.graphics;
 	exports drehfraehsim.view to javafx.graphics;
