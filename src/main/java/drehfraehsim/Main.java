6715da27308672271bc13e0f7442147e3980542d
Mark Joachim Krallmann <joachim.krallmann@protonmail.com>
2022-06-16 17:25:21 +0200

    FIX JavaFX rendern auf Linux mit AMD Graka

--- src/main/java/drehfraehsim/Main.java
+++ src/main/java/drehfraehsim/Main.java
@@ -2,6 +2,15 @@ package drehfraehsim;
 
 public class Main {
 	public static void main(String[] args) {
+		// JavaFX weigert sich sonst bei Linux mit AMD Graka 🙄
+		if (aufLinux()) {
+			System.setProperty("prism.forceGPU", "true"); 
+		}
+		
 		JavaFXStart.launch(JavaFXStart.class, args);
+	}
+	
+	private static final boolean aufLinux() {
+		return System.getProperty("os.name").toLowerCase().contains("linux");
 	}
 }
