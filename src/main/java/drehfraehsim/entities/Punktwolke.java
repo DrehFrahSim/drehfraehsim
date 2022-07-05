9c5048b94f9fe2a6d176ba55b1254723dc58ad67
Joachim Krallmann <joachim.krallmann@tu-dortmund.de>
2022-07-05 10:19:59 +0200

    Entferne quaderpunktwolke methode

--- src/main/java/drehfraehsim/entities/Punktwolke.java
+++ src/main/java/drehfraehsim/entities/Punktwolke.java
@@ -46,20 +46,6 @@ public class Punktwolke {
 		return punktwolke;
 	}
 
-	public static Punktwolke quader(double höhe, double breite, double tiefe) {
-		Punktwolke punktwolke = new Punktwolke();
-
-		for (double xInkrement = -breite/2; xInkrement <= breite/2; xInkrement = xInkrement + (1/GENAUIGKEIT)) {
-			for (double yInkrement = 0; yInkrement <= höhe; yInkrement = yInkrement + (1/GENAUIGKEIT)) {
-				for (double zInkrement = 0; zInkrement >= -tiefe; zInkrement = zInkrement - (1/GENAUIGKEIT)) {
-						punktwolke.punktHinzufügen(new Vector3(xInkrement, yInkrement, zInkrement));
-				}
-			}
-		}
-
-		return punktwolke;
-	}
-
 	private void punktHinzufügen(Vector3 punkt) {
 		punkte.add(punkt);
 	}
